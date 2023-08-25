52915e2016711a8e89eac351f56cee1abb3cf07b
Laura <Laura@DESKTOP-BESSB6E>
2023-08-25 16:51:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Iveskite skaiciu A, B ir C reiksmes: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int positiveNumber = 0;
+int sum = 0;
+
+if (numberA >0)
+{
+    positiveNumber++;
+    sum += numberA;
+}
+if(numberB >0)
+{
+    positiveNumber++;
+    sum += numberB;
+}
+if (numberC >0)
+{
+    positiveNumber++;
+    sum += numberC;
+}
+
+if (positiveNumber >= 2)
+{
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Neįmanoma suskaičiuoti sumos");
+}
+
+
+//if ((numberA > 0 && numberB > 0) ||
+//    (numberB > 0 && numberC > 0) ||
+//    (numberA > 0 && numberC > 0) ||
+//    (numberA > 0 && numberB > 0 && numberC > 0))
+
+//    {
+//    int sum = numberA + numberB + numberC;
+//    Console.WriteLine(sum);
+//    }
+//else
+//{
+//    Console.WriteLine("Neįmanoma suskaičiuoti sumos");
+//}
